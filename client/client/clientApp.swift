3ca1eb2daaf98a96d5411d1e81ba993b493742d0
emircan duman <mail@eduman.de>
2024-11-06 17:36:40 +0100

    #initial setup of client

---
+++ client/client/clientApp.swift
@@ -0,0 +1,32 @@
+//
+//  clientApp.swift
+//  client
+//
+//  Created by Emircan Duman on 06.11.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct clientApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
